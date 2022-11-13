e111f8b0748721ffec72b5548e033fd185600009
repp2205 <ALDEAMO\dev10@user-asus-expertbook-p2451fa-p2451fa.aldeamo.dom>
2022-11-13 13:24:51 -0500

    Se corrige bug

--- src/main/java/com/app/breadapp/controllers/BranchOfficeController.java
+++ src/main/java/com/app/breadapp/controllers/BranchOfficeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "")
 @RestController
-@RequestMapping(value = "/brachOffice")
+@RequestMapping(value = "/branchOffice")
 @Log4j2
 public class BranchOfficeController {
 
